381261de3bdc3511b5ea1ae589feb619a4155d0f
abigailmurray <abigailmurray25@gmail.com>
2023-12-04 10:36:49 -0500

    Update video_capture to display letter on screen and speak the letter

--- video_capture.py
+++ video_capture.py
@@ -1,5 +1,7 @@
 import cv2
 import torch
+import time
+import pyttsx3
 import numpy as np
 import mediapipe as mp
 from models import ASLCNN
@@ -7,14 +9,14 @@ from PIL import Image
 from data import transform
 
 # Class labels for the ASL signs
-# class_labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'SPACE',
-#                 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 class_labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'space']
 
 cv2.namedWindow('Hand Detection and Classification', cv2.WINDOW_NORMAL)
 cv2.namedWindow('Cropped Hand Region', cv2.WINDOW_NORMAL)
 frame_count = 0
 
+# Initialize text-to-speech engine
+engine = pyttsx3.init()
 
 def crop_hand_region(frame, hand_landmarks, target_size=(200, 200), padding=20):
     try:
@@ -41,6 +43,24 @@ def crop_hand_region(frame, hand_landmarks, target_size=(200, 200), padding=20):
         return None
 
 
+def text_to_speech(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def get_ground_truth_label(frame_count):
+    return class_labels[frame_count % len(class_labels)]
+
+
+def evaluate_accuracy(detected_sign, ground_truth_label, confidence, correct_predictions, total_predictions, confidence_threshold=0.7):
+    # Check if the confidence is above the threshold
+    if confidence > confidence_threshold:
+        total_predictions += 1
+        if detected_sign == ground_truth_label:
+            correct_predictions += 1
+
+    return correct_predictions, total_predictions
+
 def main():
     # Initialize MediaPipe Hands module
     mp_hands = mp.solutions.hands
@@ -58,8 +78,13 @@ def main():
 
     # Hand region target size = 200 x 200 pixels
     target_size = (200, 200)
+    correct_predictions = 0
+    total_predictions = 0
+    total_confidence = 0
 
     global frame_count
+    detected_sign = ""
+    last_detection_time = time.time()
 
     while cap.isOpened():
         ret, frame = cap.read()
@@ -75,9 +100,6 @@ def main():
         # Check if hands are detected
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
-                # Draw landmarks on hand
-                # mp.solutions.drawing_utils.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-
                 # Capture an image every 20 frames
                 if frame_count % 20 == 0:
                     hand_region = crop_hand_region(frame, hand_landmarks, target_size=target_size, padding=50)
@@ -88,32 +110,48 @@ def main():
                         hand_pil = Image.fromarray(cv2.cvtColor(hand_region, cv2.COLOR_BGR2RGB))
                         hand_region_tensor = transform(hand_pil)
 
-                        # Convert the cropped hand region to a PyTorch tensor and normalize
-                        # hand_region_tensor = torch.from_numpy(hand_region.transpose((2, 0, 1))).float() / 255.0
-
                         # Forward pass through the ASL classification model
                         with torch.no_grad():
                             out = model(hand_region_tensor.unsqueeze(0).to(device))
 
-                        # Print the raw output from the model
-                        print("Raw Model Output:", out)
-
                         # Get the predicted class
                         _, pred = torch.max(out.data, 1)
+                        confidence = torch.nn.functional.softmax(out, dim=1)[0][pred.item()].item()
+
                         detected_sign = class_labels[pred.item()]
                         print("Detected ASL Sign:", detected_sign)
 
-        # Display the original frame with landmarks
-        cv2.imshow('Hand Detection and Classification', frame)
+                        # Update the last detection time
+                        last_detection_time = time.time()
+
+                        # Speak out the detected sign
+                        text_to_speech(detected_sign)
+
+                        ground_truth_label = get_ground_truth_label(frame_count)
+                        correct_predictions, total_predictions = evaluate_accuracy(detected_sign, ground_truth_label,
+                                                                                   confidence, correct_predictions,
+                                                                                   total_predictions)
+
+                        # Draw the detected ASL sign on the frame if it's within the 3-second window
+                        if time.time() - last_detection_time <= 3:
+                            cv2.putText(frame, f"ASL Sign: {detected_sign}", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                                        (0, 255, 0), 2,
+                                        cv2.LINE_AA)
+
+                        # Display the original frame with landmarks
+                        cv2.imshow('Hand Detection and Classification', frame)
+
 
         if cv2.waitKey(1) & 0xFF == 27:  # Press 'Esc' to exit
+            # Reset frame_count and last_detection_time
+            frame_count = 0
+            last_detection_time = time.time()
             break
 
         frame_count += 1
 
     cap.release()
     cv2.destroyAllWindows()
-
 
 if __name__ == '__main__':
     main()
