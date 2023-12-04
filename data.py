aa9b80c641e5df0f542026ae25a0eee025b19082
Matt D <dirisiom@msu.edu>
2023-12-04 18:02:54 -0500

    changed data path, changed manual test to use real world data, and added padding to vid cap

--- data.py
+++ data.py
@@ -1,12 +1,4 @@
-import numpy as np
-import pandas as pd
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torchvision import datasets, transforms, models
-from torch.utils.data import DataLoader, random_split
-import matplotlib.pyplot as plt
-
+from torchvision import datasets, transforms
 
 transform = transforms.Compose([
     transforms.Resize((200, 200)),
@@ -18,7 +10,6 @@ transform = transforms.Compose([
 
 
 def fetch_data():
-    # path = './data/asl_images'
-    path = '/Users/dattm/Downloads/asl_big_set/ASL_Alphabet_Dataset'
+    path = './data/asl_big_set/ASL_Alphabet_Dataset'
     dset = datasets.ImageFolder(root=path, transform=transform)
     return dset
--- manual_test.py
+++ manual_test.py
@@ -1,10 +1,14 @@
 import cv2
+import matplotlib.pyplot as plt
+import mediapipe as mp
+import numpy as np
 import torch
+import os
+
 from PIL import Image
 from torchvision import transforms
 
 from models import ASLCNN
-import matplotlib.pyplot as plt
 
 transform = transforms.Compose([
     transforms.Resize((200, 200)),
@@ -12,13 +16,40 @@ transform = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
-class_labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'SPACE', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+class_labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+                'V', 'W', 'X', 'Y', 'Z', 'space']
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = ASLCNN(len(class_labels)).to(device)
 model_path = 'data/asl_classifier_state_dict.pth'
 model.load_state_dict(torch.load(model_path, map_location=device))
 model.eval()
+
+
+def crop_hand_region(frame, hand_landmarks, target_size=(200, 200), padding=100):
+    try:
+        # Extract the bounding box from the hand landmarks
+        points = np.array(
+            [(int(lm.x * frame.shape[1]), int(lm.y * frame.shape[0])) for lm in hand_landmarks.landmark])
+        bBox = cv2.boundingRect(points)
+
+        if bBox[2] > 0 and bBox[3] > 0:
+            x, y, w, h = bBox
+            x -= padding
+            y -= padding
+            w += 2 * padding
+            h += 2 * padding
+
+            # Crop and resize the hand region
+            hand_region = frame[max(0, y):min(frame.shape[0], y + h), max(0, x):min(frame.shape[1], x + w)]
+            hand_region = cv2.resize(hand_region, target_size)
+
+            return hand_region
+        else:
+            return None
+    except Exception as e:
+        print(f"Error in crop_hand_region: {e}")
+        return None
 
 
 # Function to preprocess and transform the image
@@ -30,33 +61,48 @@ def preprocess_and_transform_image(image_path):
 
     # Convert BGR to RGB (as OpenCV loads in BGR format)
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    # plt.imshow(image_rgb)
+    # plt.show()
 
-    # Convert the image to PIL format for applying transforms
-    image_pil = Image.fromarray(image_rgb)
-    cropped = image_pil.crop((5,5,195,195))
+    hands = mp.solutions.hands.Hands()
+    results = hands.process(image_rgb)
+    if results.multi_hand_landmarks:
+        region = crop_hand_region(image_rgb, results.multi_hand_landmarks[0])
+        image_pil = Image.fromarray(cv2.cvtColor(region, cv2.COLOR_BGR2RGB))
 
     # Apply the transformations
-    image_tensor = transform(cropped)
+    image_tensor = transform(image_pil)
 
-    return image_tensor, cropped
+    return image_tensor, image_pil
 
 
 # Function to predict the class of an image
-def classify_image(image_tensor):
+def classify_image(i_t):
     with torch.no_grad():
-        image_tensor = image_tensor.unsqueeze(0).to(device)  # Add batch dimension and send to device
-        output = model(image_tensor)
+        i_t = i_t.unsqueeze(0).to(device)  # Add batch dimension and send to device
+        output = model(i_t)
         _, predicted = torch.max(output, 1)
         return class_labels[predicted.item()]
 
+dpath = './data/RealWorldData'
+correct = 0
+total = 0
+for dir in os.listdir(dpath):
+    label = dir
+    for im in os.listdir(f'{dpath}/{dir}'):
+        total += 1
+        image_path = F"{dpath}/{dir}/{im}"
+        image_tensor, transformed_image_pil = preprocess_and_transform_image(image_path)
+        predicted_class = classify_image(image_tensor)
+        print(predicted_class)
+        if predicted_class == label:
+            correct += 1
+        # if label == 'A':
+        #     # Display the preprocessed image
+        #     plt.imshow(transformed_image_pil)
+        #     plt.title(f"Predicted Class: {predicted_class}")
+        #     plt.show()
 
-# Replace 'path_to_your_image.jpg' with the path to the image you want to classify
-image_path = './data/asl_images/SPACE/space/space2.jpg'
-image_tensor, transformed_image_pil = preprocess_and_transform_image(image_path)
-predicted_class = classify_image(image_tensor)
-print(predicted_class)
+print(f"{correct} correct out of {total}")
 
-# Display the preprocessed image
-plt.imshow(transformed_image_pil)
-plt.title(f"Predicted Class: {predicted_class}")
-plt.show()
+
